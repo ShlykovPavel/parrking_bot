417f515ab56bcdd572db0f56808fda47eaa39de0
ShlykovPavel <pavel14640@gmail.com>
2025-04-03 15:04:05 +0300

    add terminate app, after dead thread

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import logging
 import sys
+import time
+
+import requests
 import telebot
 import threading
 import sqlite3
@@ -8,7 +11,6 @@ from reminder.remind_schedule import run_scheduler
 import os
 from dotenv import load_dotenv
 from database import ensure_db_exists
-
 
 db_path = 'Database/parking.db'
 load_dotenv()
@@ -25,20 +27,29 @@ user_data = {}
 bot_commands = Bot_commands(bot, user_data)
 bot_commands.register_handlers()
 
+error_event = threading.Event()
 # Функция для запуска бота
-def run_bot():
-    try:
-        print("Запуск бота...")
-        bot.infinity_polling()
-    except Exception as e:
-        logging.error(f"Произошла ошибка в основном потоке бота: {e}\n Аварийное завершение бота")
-        sys.exit(1)
+def run_bot(bot):
+    print("Запуск бота...")
+    while True:
+        try:
+            bot.polling(non_stop=True)
+        except requests.exceptions.ReadTimeout:
+            logging.error("Произошла ошибка в основном потоке бота: ReadTimeout. Перезапуск через 60 секунд")
+            time.sleep(60)
+
+        except Exception as e:
+            logging.error(f"1Произошла ошибка в основном потоке бота: {e}\n Аварийное завершение бота")
+            error_event.set()
+            # print(f'error_event.set() отработал')
+            return
+
 
 if __name__ == '__main__':
     # Создание потоков для бота и планировщика
     try:
-        bot_thread = threading.Thread(target=run_bot)
-        scheduler_thread = threading.Thread(target=run_scheduler, args=(bot, db_cursor))
+        bot_thread = threading.Thread(target=run_bot, args=(bot,), daemon=True)
+        scheduler_thread = threading.Thread(target=run_scheduler, args=(bot, db_cursor), daemon=True)
 
         # Запуск потоков
         bot_thread.start()
@@ -46,6 +57,21 @@ if __name__ == '__main__':
 
         # Печать сообщения о запуске
         print("Бот и планировщик запущены")
+
+        while True:
+            if not bot_thread.is_alive() or scheduler_thread.is_alive():
+                if error_event.is_set():
+                    logging.error("Обнаружена ошибка в одном из потоков. Аварийное завершение")
+                    # raise Exception
+                    # sys.exit(1)
+                time.sleep(0.1)  # Проверяем каждую секунду
+
+            if error_event.is_set():
+                logging.error("Один из потоков завершился с ошибкой. Аварийное завершение программы")
+                # sys.exit(1)
+                raise Exception
+
+
     except Exception as e:
-        logging.error(f"Произошла ошибка в основном потоке: {e}\n Аварийное завершение бота")
+        logging.error(f"2Произошла ошибка в основном потоке: {e}\n Аварийное завершение бота")
         sys.exit(1)
--- reminder/remind_schedule.py
+++ reminder/remind_schedule.py
@@ -1,8 +1,11 @@
 import logging
 import sys
+import threading
 import time
 from datetime import datetime
 from functools import partial
+# from main import *
+import requests
 import schedule
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 import holidays
@@ -33,17 +36,33 @@ def check_reminders(bot, db_cursor, country_code='RU'):
         yes_button = InlineKeyboardButton(text="Да", callback_data=f"reminder_yes")
         no_button = InlineKeyboardButton(text="Нет", callback_data=f"reminder_no")
         keyboard.add(yes_button, no_button)
+        i = 0
+        while i < 3:
+            try:
+                logging.info(f"Попытка # {i + 1} отправить напоминание юзеру: {chat_id}")
+                bot.send_message(chat_id, reminder_message, reply_markup=keyboard)
+                logging.info(f"Напоминание отправлено пользователю {chat_id}")
+                break
+            except ApiTelegramException as e:
+                if "bot was blocked by the user" in str(e):
+                    logging.warning(f"Напоминание не отправлено. Пользователь {chat_id} заблокировал бота.")
+                    break
+                else:
+                    logging.error(f"Ошибка при отправке сообщения пользователю {chat_id}: {e}")
+                    break
+            except requests.exceptions.ReadTimeout:
+                logging.error(f"Произошла ошибка в потоке планировщика бота: ReadTimeout. Ожидание 10 секунд. \nПопытка: {i}")
+                time.sleep(10)
+                i += 1
+                if i == 3:  # После трёх попыток
+                    logging.error(f"Исчерпаны 3 попытки для {chat_id}. ")
+                    raise Exception("Max retries exceeded for ReadTimeout")
 
-        try:
-            bot.send_message(chat_id, reminder_message, reply_markup=keyboard)
-            logging.info(f"Напоминание отправлено пользователю {chat_id}")
-        except ApiTelegramException as e:
-            if "bot was blocked by the user" in str(e):
-                logging.warning(f"Напоминание не отправлено. Пользователь {chat_id} заблокировал бота.")
-            else:
-                logging.error(f"Ошибка при отправке сообщения пользователю {chat_id}: {e}")
+            except Exception as e:
+                logging.error(f"Ошибка при отправке сообщения пользователю {chat_id}: {e}\n Попытка: {i}")
+                i += 1
 
-
+error_event = threading.Event()
 def run_scheduler(bot, db_cursor):
     try:
         schedule.every(1).minutes.do(partial(check_reminders, bot, db_cursor))
@@ -53,4 +72,5 @@ def run_scheduler(bot, db_cursor):
             time.sleep(30)
     except Exception as e:
         logging.error(f"Произошла ошибка в потоке планировщика бота: {e}\n Аварийное завершение бота")
+        error_event.set()
         sys.exit(1)
